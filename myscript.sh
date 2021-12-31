d7cd32d520b0809dd811a62163f2ee2b92fbd3ae
Kiran <kiran@yahoo.com>
2021-12-31 07:04:09 +0530

    reverted to earlier version

--- myscript.sh
+++ myscript.sh
@@ -8,11 +8,3 @@ echo "Hello World"
 echo "The time now is $(date | cut -d ' ' -f 5,6)"
 echo " Connecting to $Web_IP"
 scp -i $PRIVATE_KEY -r /path/to/files ubuntu@$Web_IP:~/
-
-
-echo "let's discsonnect and udpate"
-
-if [ -z $username]
-then
-    echo "No username provided"
-fi
