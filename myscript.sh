1ce7592b6394535c156611dc54eb5dd6a5121729
Kiran <kiran@yahoo.com>
2021-12-31 07:00:15 +0530

    updated env values, myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -8,3 +8,6 @@ echo "Hello World"
 echo "The time now is $(date | cut -d ' ' -f 5,6)"
 echo " Connecting to $Web_IP"
 scp -i $PRIVATE_KEY -r /path/to/files ubuntu@$Web_IP:~/
+
+
+echo "let's discsonnect and udpate"
